8ee6f92c2063484d5a6cc60e6884a5214458f790
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-12 00:33:24 +0300

    chore: set up frontend project structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components";
+
+const App = () => {
+  return (
+    <div className="text-red-500">
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
