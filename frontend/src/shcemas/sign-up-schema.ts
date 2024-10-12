d3e0dea3bfd508bc20310763243949c42a9cf4e7
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-12 10:18:28 +0300

    feat: add sign-up schema for sign-up form

---
+++ frontend/src/shcemas/sign-up-schema.ts
@@ -0,0 +1,21 @@
+import { MIN_PASSWORD } from "@/global";
+import * as yup from "yup";
+
+export const SignUpSchema = yup.object({
+  name: yup
+    .string()
+    .min(2, "Name field is required")
+    .required("Name field is required"),
+  email: yup.string().email().required("Invalid email"),
+  password: yup
+    .string()
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter.")
+    .matches(/[0-9]/, "The password must contain at least one number.")
+    .min(
+      MIN_PASSWORD,
+      `Password must contain at least ${MIN_PASSWORD} characters.`
+    )
+    .required(),
+});
+
+export type ISignUpSchema = yup.InferType<typeof SignUpSchema>;
