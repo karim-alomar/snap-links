b77b3fa90af28e593935427496165308be950bdb
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-12 17:23:29 +0300

    chore: add secret file to manage env keys

---
+++ backend/src/secret.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+export const PORT = process.env.PORT;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY!;
+export const REBRANDLY_ENDPOINT = process.env.REBRANDLY_ENDPOINT!;
+export const REBRANDLY_API_KEY = process.env.REBRANDLY_API_KEY!;
