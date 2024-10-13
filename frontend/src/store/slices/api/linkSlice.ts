db00ebe3ab8b2ae25e3801746f5923209d9be079
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 13:39:04 +0300

    feat(redux): create links slice

---
+++ frontend/src/store/slices/api/linkSlice.ts
@@ -0,0 +1,52 @@
+import { APIActionResponse, APIResponse, LinkType } from "@/types";
+import { apiSlice } from "./apiSlice";
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  overrideExisting: true,
+  endpoints: (builder) => ({
+    fetchLinks: builder.query<APIResponse<LinkType>, void>({
+      query: () => ({
+        url: "/links",
+        method: "GET",
+      }),
+    }),
+    createLink: builder.mutation<APIActionResponse<LinkType>, { url: string }>({
+      query: (data) => ({
+        url: "/links",
+        method: "POST",
+        data,
+      }),
+    }),
+    updateLink: builder.mutation<
+      APIActionResponse<LinkType>,
+      { id?: number; url: string }
+    >({
+      query: ({ id, ...data }) => ({
+        url: `/links/${id}`,
+        method: "PUT",
+        data,
+      }),
+    }),
+    deleteLink: builder.mutation<APIActionResponse<LinkType>, { id: number }>({
+      query: ({ id }) => ({
+        url: `/links/${id}`,
+        method: "DELETE",
+      }),
+    }),
+    clickLink: builder.mutation<APIActionResponse<LinkType>, { id: number }>({
+      query: ({ id }) => ({
+        url: `/links/${id}/click-link`,
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useFetchLinksQuery,
+  useCreateLinkMutation,
+  useUpdateLinkMutation,
+  useDeleteLinkMutation,
+  useClickLinkMutation,
+  util: { updateQueryData: updateLinksQueryData },
+} = extendedApiSlice;
