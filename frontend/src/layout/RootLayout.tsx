ad2277bed17c78542090e1872a5acef52ee1fbb3
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 14:27:52 +0300

    feat: add loading to rootLayout

--- frontend/src/layout/RootLayout.tsx
+++ frontend/src/layout/RootLayout.tsx
@@ -1,11 +1,22 @@
 import { Navbar } from "@/components";
+import { useAppSelector } from "@/hooks";
+import { Loader2 } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
+  const { status } = useAppSelector(({ user }) => user);
   return (
     <div className="container">
-      <Navbar />
-      <Outlet />
+      {status === "loading" ? (
+        <div className="flex items-center justify-center h-[calc(100vh-100px)]">
+          <Loader2 className="animate-spin" size={35} />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      )}
     </div>
   );
 };
