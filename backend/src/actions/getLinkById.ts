e8818b76f81e7d75f5ec0404f0e64dda05e01127
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 13:17:38 +0300

    chore: minor change in getLinkById action

--- backend/src/actions/getLinkById.ts
+++ backend/src/actions/getLinkById.ts
@@ -2,7 +2,7 @@ import { db } from "../../db";
 
 export const getLinkById = async (id: number) => {
   try {
-    const link = await db.links.findUnique({
+    const link = await db.link.findUnique({
       where: {
         id,
       },
