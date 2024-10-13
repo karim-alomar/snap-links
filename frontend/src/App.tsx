239e911fd93d17d0e36b89c0f0a730378ba9f358
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 19:26:16 +0300

    feat: add greeting text to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { LinkShortenerForm, ShortenedLinkTable } from "@/components";
 import { LinkType } from "@/types";
 import { useState } from "react";
+import { useAppSelector } from "./hooks";
 
 const App = () => {
   const [linkEditState, setLinkEditState] = useState<{
@@ -10,11 +11,20 @@ const App = () => {
     link: undefined,
     mode: "create",
   });
+  const { user } = useAppSelector(({ user }) => user);
 
   return (
-    <div className="mt-10">
-      <LinkShortenerForm linkEditState={linkEditState} />
-      <ShortenedLinkTable setLinkEditState={setLinkEditState} />
+    <div className="mt-5 space-y-3">
+      {user && (
+        <div className="flex flex-col items-start justify-start">
+          <span className="text-2xl font-bold">Hi, {user?.name}👋</span>
+          <span className="text-black/50">{user.email}</span>
+        </div>
+      )}
+      <div>
+        <LinkShortenerForm linkEditState={linkEditState} />
+        <ShortenedLinkTable setLinkEditState={setLinkEditState} />
+      </div>
     </div>
   );
 };
