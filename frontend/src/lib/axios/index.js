d9234f81681534fd7fc1b9c5539cf637ff6f0f43
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 13:42:01 +0300

    fix: updated axios file to improve cookie sending

--- frontend/src/lib/axios/index.js
+++ frontend/src/lib/axios/index.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
-
+import Cookies from "js-cookie";
 export const axios = Axios.create({
-  baseURL: `https://jsonplaceholder.typicode.com/`,
+  baseURL: `http://localhost:3000/api`,
   withCredentials: false,
   headers: {
     Accept: "application/json",
@@ -9,3 +9,23 @@ export const axios = Axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axios.interceptors.request.use(
+  (config) => {
+    const guestId = Cookies.get("guest_id");
+    const accessToken = Cookies.get("access_token");
+
+    if (guestId) {
+      config.headers.guest_id = guestId;
+    }
+
+    if (accessToken) {
+      config.headers.access_token = accessToken;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
