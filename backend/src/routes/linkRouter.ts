84b5cfef6a13e7f424aafe8a25afad724e1409a6
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 13:23:40 +0300

    feat(backend): add routes for fetching links and clicking links

--- backend/src/routes/linkRouter.ts
+++ backend/src/routes/linkRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { createLink, updateLink, deleteLink } from "../controllers";
+import {
+  createLink,
+  updateLink,
+  deleteLink,
+  fetchLinks,
+  clickLink,
+} from "../controllers";
 
 const linkRouter: Router = Router();
 
+linkRouter.get("/links", fetchLinks);
 linkRouter.post("/links", createLink);
 linkRouter.put("/links/:id", updateLink);
 linkRouter.delete("/links/:id", deleteLink);
+linkRouter.post("/links/:id/click-link", clickLink);
 
 export default linkRouter;
