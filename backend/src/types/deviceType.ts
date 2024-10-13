2cb957a385a519ac8266a957b1fb0b8ca0646170
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 13:24:28 +0300

    feat(types): add DeviceType interface

---
+++ backend/src/types/deviceType.ts
@@ -0,0 +1,9 @@
+export interface DeviceType {
+  isMobile: boolean;
+  isTablet: boolean;
+  isAndroidTablet: boolean;
+  isDesktop: boolean;
+  isSmartTV: boolean;
+  isBot: boolean;
+  isCurl: boolean;
+}
