e71c558144932ae8fd51aa7b21a38e4baeeef5ec
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-13 15:34:43 +0300

    feat: add expiry_time key to LinkShortenerSchema

--- frontend/src/shcemas/link-shortener-schema.ts
+++ frontend/src/shcemas/link-shortener-schema.ts
@@ -2,6 +2,7 @@ import * as yup from "yup";
 
 export const LinkShortenerSchema = yup.object({
   url: yup.string().url().required(),
+  expiry_time: yup.number().nullable(),
 });
 
 export type ILinkShortenerSchema = yup.InferType<typeof LinkShortenerSchema>;
