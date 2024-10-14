3daabd6dd349d94d7ca80dd111ad1b55e4a54ac4
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-14 22:50:33 +0300

    chore: change type of expiry_time in link slice

--- frontend/src/store/slices/api/linkSlice.ts
+++ frontend/src/store/slices/api/linkSlice.ts
@@ -12,7 +12,7 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
     }),
     createLink: builder.mutation<
       APIActionResponse<LinkType>,
-      { url: string; expiry_time?: number | null }
+      { url: string; expiry_time?: number | string | null }
     >({
       query: (data) => ({
         url: "/links",
@@ -22,7 +22,7 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
     }),
     updateLink: builder.mutation<
       APIActionResponse<LinkType>,
-      { id?: number; url: string; expiry_time?: number | null }
+      { id?: number; url: string; expiry_time?: number | string | null }
     >({
       query: ({ id, ...data }) => ({
         url: `/links/${id}`,
