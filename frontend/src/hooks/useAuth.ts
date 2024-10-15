6702d1a99a4320be9edd5e7514f9bd2927ed463f
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-15 21:56:46 +0300

    chore: remove unused code

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,17 +1,13 @@
-import { setUser } from "@/store/slices/user-slice";
 import { User } from "@/types";
 import Cookies from "js-cookie";
-import { useAppDispatch } from "@/hooks";
 import { useNavigate } from "react-router-dom";
 
 export const useAuth = () => {
-  const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
   const login = (data: User) => {
     Cookies.set("access_token", data.accessToken);
     Cookies.remove("guest_id");
-    dispatch(setUser(data));
     navigate("/");
     window.location.reload();
   };
