48f2a0a63b669c5c373573d3381760ef29224340
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-15 21:18:34 +0300

    fix: remove access_token and guest_id from cookies on user data fetch error

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { useAppDispatch } from "@/hooks";
+import { useAppDispatch, useAppSelector } from "@/hooks";
 import { fetchAuth } from "@/store/slices/user-slice";
 import Cookies from "js-cookie";
 import { createContext, ReactNode, useEffect } from "react";
@@ -9,14 +9,19 @@ interface Props {
   children: ReactNode;
 }
 export const AuthProvider = ({ children }: Props) => {
+  const { status } = useAppSelector(({ user }) => user);
   const dispatch = useAppDispatch();
   const token = Cookies.get("access_token");
 
   useEffect(() => {
     if (token) {
       dispatch(fetchAuth());
+      if (status === "failed") {
+        Cookies.remove("access_token");
+        Cookies.remove("guest_id");
+      }
     }
-  }, [dispatch, token]);
+  }, [dispatch, token, status]);
 
   const value = {};
   return <authContext.Provider value={value}>{children}</authContext.Provider>;
