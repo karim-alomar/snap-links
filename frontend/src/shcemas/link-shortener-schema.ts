8f02556be758e3b202e3a7844187f2a877d27e7d
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-15 22:35:19 +0300

    chore: change type of expiry_time in LinkShortenerSchema

--- frontend/src/shcemas/link-shortener-schema.ts
+++ frontend/src/shcemas/link-shortener-schema.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const LinkShortenerSchema = yup.object({
   url: yup.string().url().required(),
-  expiry_time: yup.string().nullable(),
+  expiry_time: yup.date().nullable(),
 });
 
 export type ILinkShortenerSchema = yup.InferType<typeof LinkShortenerSchema>;
