8b8757e8ccb2675e45dfe8af399d01053b06da84
Karim Alomar <47941750+karim-alomar@users.noreply.github.com>
2024-10-19 13:40:44 +0300

    fix: minor fix

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,3 +1,3 @@
-export { default as Profile } from "./Profile";
+export { default as ProfileForm } from "./ProfileForm";
 export { default as LoginForm } from "./LoginForm";
 export { default as RegisterForm } from "./RegisterForm";
